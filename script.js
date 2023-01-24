f963c8eac4ea6ab9c0b81c8cbcce0ef19e9de656
Meder-Karakol <akosta3922@gmail.com>
2023-01-24 21:53:10 +0700

    exam

---
+++ script.js
@@ -0,0 +1,43 @@
+const arr = [ "1", "43", "23", "54", "2", "3", "89"]
+const str = arr.join(" ")
+console.log(str)
+
+const arr = [ "1", "43", "23", "54", "2", "3", "89"]
+let max =arr[0]
+let min =arr[0]
+for(let i = 0; i< arr.length; i++){
+    if(max<arr[i]){
+        max=arr[i]
+    }
+    if(min>arr[i]){
+        min=arr[i]
+    }
+
+}
+console.log(max)
+console.log(min)
+
+
+
+const arr = [ 1, 43, 23, 54, 2, 3, 89]
+
+let even = 0
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+        //even += arr[i]
+    }
+}
+console.log(even)
+
+
+const arr = [ "1", "43", "23", "54", "2", "3", "89"]
+for (let i = arr.length - 1; i >= 0; i--) {
+    console.log(arr[i])
+    for (let i = arr.length - 1; i >= 0; i--) {
+        console.log(arr[i])
+    }
+
+    const arr = [ "1", "43", "23", "54", "2", "3", "89"]
+    const number =arr.join(" ")
+    console.log(number)
